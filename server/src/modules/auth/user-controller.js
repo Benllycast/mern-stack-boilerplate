2aaccfb66d8b5cee6bf24368856df352d84e0a01
Benllycast <benllycast@gmail.com>
2022-08-30 13:18:37 -0500

    Initialize feature/introduce-createdby-modifiedby

---
+++ server/src/modules/auth/user-controller.js
@@ -0,0 +1,136 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const {
+  update,
+  deleteById,
+  getById,
+  search,
+  count,
+  tryCreateUser,
+  searchOne,
+} = require("./service");
+const { validateUserUpdate, validateUserCreate } = require("./request");
+const { handleValidation } = require("../../common/middlewares");
+const { NotFound } = require("../../common/errors");
+
+const router = express.Router();
+const ModelName = "User";
+
+const getHandler = async (req, res, next) => {
+  try {
+    const items = [
+      { id: 1, name: "User 1" },
+      { id: 2, name: "User 2" },
+    ];
+    const result = {
+      data: items,
+      total: items.length,
+      success: true,
+    };
+    return res.status(200).send(result);
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const getByIdHandler = async (req, res, next) => {
+  try {
+    const { id } = req.query;
+    const item = await getById(id, req.user.id);
+    if (item) {
+      return res.status(200).send(item);
+    }
+    throw new NotFound(`${ModelName} not found by the id: ${id}`);
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const postHandler = async (req, res, next) => {
+  try {
+    const user = req.body;
+    const id = await tryCreateUser(user);
+    if (!id) {
+      return res.status(400).send({
+        status: "error",
+        message: "User already exists by username or email or phone number.",
+      });
+    }
+    return res
+      .status(201)
+      .send({ status: "ok", message: "User created successfully", id });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const searchHandler = async (req, res, next) => {
+  try {
+    if (!req.body.pageSize) {
+      req.body.pageSize = 10;
+    }
+    if (!req.body.current) {
+      req.body.current = 1;
+    }
+    const query = { ...req.body, userId: req.user.id };
+    const result = await search(query);
+    const response = { success: true, ...result };
+    return res.status(200).send(response);
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const countHandler = async (req, res, next) => {
+  try {
+    const query = { ...req.body, userId: req.user.id };
+    const result = await count(query);
+    const response = { success: true, ...result };
+    return res.status(200).send(response);
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const putHandler = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const id = await update(body, ModelName);
+    return res.status(200).send(id);
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const deleteHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await deleteById(id, ModelName);
+    return res
+      .status(200)
+      .send({ success: true, message: `${ModelName} deleted` });
+  } catch (error) {
+    return next(error, req, res);
+  }
+};
+
+const checkUserHandler = async (req, res) => {
+  if (req.body) {
+    const user = await searchOne(req.body, ModelName);
+    if (user) {
+      return res.status(200).send({ status: "success", message: "User found" });
+    }
+  }
+  return res.status(200).send({ status: "error", message: "User not found" });
+};
+
+router.get("/", getHandler);
+router.get("/detail", getByIdHandler);
+router.post("/create", handleValidation(validateUserCreate), postHandler);
+router.put("/update", handleValidation(validateUserUpdate), putHandler);
+router.post("/search", searchHandler);
+router.post("/count", countHandler);
+router.delete("/delete/:id", deleteHandler);
+router.post("/check", checkUserHandler);
+
+module.exports = router;
