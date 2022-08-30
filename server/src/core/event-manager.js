7c2d672376214edc3c7638193b377cd30af40e50
Benllycast <benllycast@gmail.com>
2022-08-30 13:18:33 -0500

    Initialize chore/eslint-fix/seed-products

---
+++ server/src/core/event-manager.js
@@ -0,0 +1,14 @@
+class EventManager {
+  constructor() {
+    if (!EventManager.instance) {
+      const EventEmitter = require("events");
+      EventManager.instance = new EventEmitter();
+    }
+  }
+
+  getInstance() {
+    return EventManager.instance;
+  }
+}
+
+module.exports = new EventManager();
---
+++ server/src/modules/role/request.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+
+const schema = Joi.object().keys({
+  name: Joi.string().min(2).max(30).required(),
+  isAdmin: Joi.bool().required(),
+  alias: Joi.string().required(),
+});
+
+const validate = (data) => {
+  const result = schema.validate(data);
+  data.isSuperAdmin = false;
+  result.value = data;
+  return result;
+};
+
+module.exports = { validate };
